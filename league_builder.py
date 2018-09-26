9c668f3606b386441ffd925659a55cffde914362
TrailKing <montrial.harrell@gmail.com>
2018-09-26 00:16:09 -0400

    added league builder requirements

---
+++ league_builder.py
@@ -0,0 +1,67 @@
+if __name__ == "__main__":
+    import csv
+    team1 = []
+    team2 = []
+    team3 = []
+    soccer_experience = list([])
+    soccer_inexperience = list([])
+    
+    def populate_experience():
+        counter = 0
+        while counter < experience:
+            team1.append(soccer_experience[counter])
+            team2.append(soccer_experience[counter+1])
+            team3.append(soccer_experience[counter+2])
+            counter += 3
+    
+    def populate_not_experienced():
+        counter = 0
+        while counter < not_experience:
+            team1.append(soccer_inexperience[counter])
+            team2.append(soccer_inexperience[counter+1])
+            team3.append(soccer_inexperience[counter+2])
+            counter += 3
+            
+    def write_league_rosters():
+        with open('teams.txt','a') as csvfile:
+            field_keys = ['Name','Height (inches)','Soccer Experience','Guardian Name(s)']
+            team1writer = csv.DictWriter(csvfile,field_keys,delimiter=',')
+            csvfile.write("Sharks\n")
+            team1writer.writerows(team1)
+ 
+            team2writer = csv.DictWriter(csvfile,field_keys,delimiter=',')
+            csvfile.write("Dragons\n")
+            team2writer.writerows(team2)
+            
+            team3writer = csv.DictWriter(csvfile,field_keys,delimiter=',')
+            csvfile.write("Raptors\n")
+            team3writer.writerows(team3)
+        
+    with open('soccer_players.csv', newline='') as csvfile:
+        soccer_reader = csv.DictReader(csvfile, delimiter=",")
+        rows = list(soccer_reader)
+        for row in rows:
+            if ((row['Soccer Experience'])) == 'YES':
+                soccer_experience.append(row)
+            else:
+                soccer_inexperience.append(row)
+    experience = len(soccer_experience)
+    not_experience = len(soccer_inexperience)
+    
+    populate_experience()
+    populate_not_experienced()
+    write_league_rosters()
+    
+    
+        
+    
+
+    
+#create a function that adds equal number of experienced players to each team
+#create a function that adds equal number of inexperienced players to each team
+
+                
+
+
+
+
